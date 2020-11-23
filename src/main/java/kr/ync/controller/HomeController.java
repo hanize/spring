8723773078da204f6008c81b41415b0d3a45a884
hanize <hanize2@gmail.com>
2020-11-23 10:42:24 +0900

    최초 프로젝트 생성

---
+++ src/main/java/kr/ync/controller/HomeController.java
@@ -0,0 +1,17 @@
+package kr.ync.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@GetMapping(value = "/")
+	public String home() {
+		return "home";
+	}
+	
+}
